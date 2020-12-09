dd8d3b08584fa35a82cf8b0bdf625f8d60fd5577
Yaroslaw Maievskij <y.maievskyi@andersenlab.com>
2020-12-09 15:51:43 +0200

    Design web api, delete instances.
    DTM-456

--- src/main/java/com/itextpdf/dito/manager/controller/instance/InstanceController.java
+++ src/main/java/com/itextpdf/dito/manager/controller/instance/InstanceController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,18 +17,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RequestMapping(InstanceController.BASE_NAME)
 public interface InstanceController {
+
     String MAJOR_VERSION = "/v1";
     String BASE_NAME = MAJOR_VERSION + "/instances";
-    String INSTANCE_NAME_ENDPOINT = "/{name}";
-    String INSTANCE_ENDPOINT = "/{socket}";
-    String INSTANCE_STATUS = INSTANCE_ENDPOINT + "/status";
 
-    @GetMapping(INSTANCE_STATUS)
+    String INSTANCE_NAME_PATH_VARIABLE = "name";
+    String INSTANCE_NAME_ENDPOINT_WITH_PATH_VARIABLE = "/{" + INSTANCE_NAME_PATH_VARIABLE + "}";
+
+    String INSTANCE_SOCKET_PATH_VARIABLE = "socket";
+    String INSTANCE_SOCKET_ENDPOINT_WITH_PATH_VARIABLE = "/{" + INSTANCE_SOCKET_PATH_VARIABLE + "}";
+    String INSTANCE_STATUS_ENDPOINT = INSTANCE_SOCKET_ENDPOINT_WITH_PATH_VARIABLE + "/status";
+
+    @GetMapping(INSTANCE_STATUS_ENDPOINT)
     @Operation(summary = "Get instance status", description = "Get instance status by socket", security = @SecurityRequirement(name = OpenApiConfig.BEARER_AUTH_SECURITY_SCHEME_NAME))
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Instance available, ready to connect"),
@@ -45,10 +48,8 @@ public interface InstanceController {
     })
     ResponseEntity<List<InstanceDTO>> saveInstances(@Valid @RequestBody InstanceCreateRequestDTO createRequestDTO);
 
-    @DeleteMapping(INSTANCE_NAME_ENDPOINT)
+    @DeleteMapping(INSTANCE_NAME_ENDPOINT_WITH_PATH_VARIABLE)
     @Operation(summary = "Disconnect instance", description = "Break communication with an instance.")
     @ApiResponse(responseCode = "200", description = "Instance disconnected successfully.")
-    ResponseEntity<Void> deleteInstance(@Parameter(description = "Encoded with base64 instance name, by which the instance will be disconnected.", allowEmptyValue = false) @PathVariable final String name);
-
-
+    ResponseEntity<Void> deleteInstance(@Parameter(description = "Encoded with base64 instance name, by which the instance will be disconnected.") @PathVariable final String name);
 }
