a0299c0408945dd5c096909910a75f03653a737e
Ivan Semenov <i.semenov@andersenlab.com>
2020-12-10 19:16:51 +0400

    Add template creation with data collection and tests
    DTM-487

--- src/test/java/com/itextpdf/dito/manager/integration/crud/TemplateFlowIntegrationTest.java
+++ src/test/java/com/itextpdf/dito/manager/integration/crud/TemplateFlowIntegrationTest.java
@@ -2,19 +2,26 @@ package com.itextpdf.dito.manager.integration.crud;
 
 import com.itextpdf.dito.manager.controller.template.TemplateController;
 import com.itextpdf.dito.manager.controller.template.TemplateTypeController;
+import com.itextpdf.dito.manager.dto.datacollection.DataCollectionType;
 import com.itextpdf.dito.manager.dto.template.create.TemplateCreateRequestDTO;
+import com.itextpdf.dito.manager.entity.TemplateEntity;
 import com.itextpdf.dito.manager.integration.AbstractIntegrationTest;
+import com.itextpdf.dito.manager.repository.datacollections.DataCollectionRepository;
 import com.itextpdf.dito.manager.repository.template.TemplateFileRepository;
 import com.itextpdf.dito.manager.repository.template.TemplateRepository;
+import com.itextpdf.dito.manager.service.datacollection.DataCollectionService;
+import org.checkerframework.checker.nullness.Opt;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 import java.io.File;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -27,15 +34,20 @@ public class TemplateFlowIntegrationTest extends AbstractIntegrationTest {
     private TemplateRepository templateRepository;
     @Autowired
     private TemplateFileRepository templateFileRepository;
+    @Autowired
+    private DataCollectionRepository dataCollectionRepository;
+    @Autowired
+    private DataCollectionService dataCollectionService;
 
     @BeforeEach
     public void clearDb() {
         templateRepository.deleteAll();
         templateFileRepository.deleteAll();
+        dataCollectionRepository.deleteAll();
     }
 
     @Test
-    public void testCreateTemplate() throws Exception {
+    public void testCreateTemplateWithoutData() throws Exception {
         TemplateCreateRequestDTO request = objectMapper.readValue(new File("src/test/resources/test-data/templates/template-create-request.json"), TemplateCreateRequestDTO.class);
         mockMvc.perform(post(TemplateController.BASE_NAME)
                 .content(objectMapper.writeValueAsString(request))
@@ -49,6 +61,22 @@ public class TemplateFlowIntegrationTest extends AbstractIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void testCreateTemplateWithData() throws Exception {
+        dataCollectionService.create("data-collection", DataCollectionType.JSON, "{\"file\":\"data\"}".getBytes(), "datacollection.json","admin@email.com");
+
+        TemplateCreateRequestDTO request = objectMapper.readValue(new File("src/test/resources/test-data/templates/template-create-request.json"), TemplateCreateRequestDTO.class);
+        request.setDataCollectionName("data-collection");
+        mockMvc.perform(post(TemplateController.BASE_NAME)
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("dataCollection").value("data-collection"));
+        Optional<TemplateEntity> template = templateRepository.findByName(request.getName());
+        assertTrue(template.isPresent());
     }
 
     @Test
