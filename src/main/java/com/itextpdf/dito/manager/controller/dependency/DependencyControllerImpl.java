b5af5d2aa85f9ffaf557dcd11756e0c26b76c231
Yaroslaw Maievskij <y.maievskyi@andersenlab.com>
2020-12-11 16:41:30 +0200

    WebApi contract: Enable display in swager UI
    DTM-474

--- src/main/java/com/itextpdf/dito/manager/controller/dependency/DependencyControllerImpl.java
+++ src/main/java/com/itextpdf/dito/manager/controller/dependency/DependencyControllerImpl.java
@@ -5,7 +5,9 @@ import com.itextpdf.dito.manager.dto.dependency.filter.DependencyFilterDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class DependencyControllerImpl extends AbstractController implements DependencyController {
     @Override
     public ResponseEntity<Page<DependencyDTO>> list(final Pageable pageable, final DependencyFilterDTO dependencyFilterDTO, final String searchParam) {
