0e67f72af27a1365a3a44d46ccffb009534ee9fe
VADZIM SAROKIN <v.sorokin@andersenlab.com>
2020-12-15 16:32:07 +0300

    Tests updating
    DTM-447

--- src/test/java/com/itextpdf/dito/manager/integration/filtering/InstanceSearchAndFilterIntegrationTest.java
+++ src/test/java/com/itextpdf/dito/manager/integration/filtering/InstanceSearchAndFilterIntegrationTest.java
@@ -45,19 +45,19 @@ public class InstanceSearchAndFilterIntegrationTest extends AbstractIntegrationT
     @Override
     @Test
     public void test_filtering() throws Exception {
-        mockMvc.perform(get(InstanceController.BASE_NAME)
+        mockMvc.perform(get(InstanceController.BASE_NAME + InstanceController.PAGEABLE_ENDPOINT)
                 .param("name", "Test"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content", hasSize(1)))
                 .andExpect(jsonPath("$.content[0].name", is(instanceEntity.getName())));
 
-        mockMvc.perform(get(InstanceController.BASE_NAME)
+        mockMvc.perform(get(InstanceController.BASE_NAME + InstanceController.PAGEABLE_ENDPOINT)
                 .param("socket", "Socket"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content", hasSize(1)))
                 .andExpect(jsonPath("$.content[0].name", is(instanceEntity.getName())));
 
-        mockMvc.perform(get(InstanceController.BASE_NAME)
+        mockMvc.perform(get(InstanceController.BASE_NAME + InstanceController.PAGEABLE_ENDPOINT)
                 .param("createdOn", "01/01/1970")
                 .param("createdOn", "01/01/1980"))
                 .andExpect(status().isOk())
@@ -67,14 +67,14 @@ public class InstanceSearchAndFilterIntegrationTest extends AbstractIntegrationT
     @Override
     @Test
     public void test_searchAndFiltering() throws Exception {
-        mockMvc.perform(get(InstanceController.BASE_NAME)
+        mockMvc.perform(get(InstanceController.BASE_NAME + InstanceController.PAGEABLE_ENDPOINT)
                 .param("name", "instance")
                 .param("search", "test"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content", hasSize(1)))
                 .andExpect(jsonPath("$.content[0].name", is(instanceEntity.getName())));
 
-        mockMvc.perform(get(InstanceController.BASE_NAME)
+        mockMvc.perform(get(InstanceController.BASE_NAME + InstanceController.PAGEABLE_ENDPOINT)
                 .param("name", "instance")
                 .param("search", "not-existing-user"))
                 .andExpect(status().isOk())
