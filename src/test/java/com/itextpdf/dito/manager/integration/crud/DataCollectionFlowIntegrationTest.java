b90ca56a7fc52325fbefbec53499748f51b4a67f
Ivan Semenov <i.semenov@andersenlab.com>
2020-12-23 11:48:22 +0400

    Temporarily disable test
    DTM-779

--- src/test/java/com/itextpdf/dito/manager/integration/crud/DataCollectionFlowIntegrationTest.java
+++ src/test/java/com/itextpdf/dito/manager/integration/crud/DataCollectionFlowIntegrationTest.java
@@ -26,6 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled
 public class DataCollectionFlowIntegrationTest extends AbstractIntegrationTest {
 
     private static final String NAME = "test-data-collection";
