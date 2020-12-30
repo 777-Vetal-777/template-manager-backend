5d1a3c6893dc593b229c5f3ce6d1134cb5130312
Sergey Nemets <s.nemets@andersenlab.com>
2020-12-30 14:00:27 +0300

    reformatted ResourceHasDependenciesException.java
    DTM-710

--- src/main/java/com/itextpdf/dito/manager/exception/resource/ResourceHasDependenciesException.java
+++ src/main/java/com/itextpdf/dito/manager/exception/resource/ResourceHasDependenciesException.java
@@ -1,15 +1,11 @@
 package com.itextpdf.dito.manager.exception.resource;
 
 public class ResourceHasDependenciesException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-
-    private final String message = "Recource has outbound dependencies";
+    private static final long serialVersionUID = 1L;
+    private static final String message = "Recource has outbound dependencies";
 
     @Override
     public String getMessage() {
         return message;
     }
-	
-	
 }
