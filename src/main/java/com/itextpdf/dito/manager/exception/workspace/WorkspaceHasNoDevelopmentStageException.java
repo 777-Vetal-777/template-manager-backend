4ece39f9e29e4ea12bbe0b8e3f5590a1bd4d0da1
Sergey Nemets <s.nemets@andersenlab.com>
2020-12-30 13:58:15 +0300

    reformatted WorkspaceHasNoDevelopmentStageException
    DTM-877

--- src/main/java/com/itextpdf/dito/manager/exception/workspace/WorkspaceHasNoDevelopmentStageException.java
+++ src/main/java/com/itextpdf/dito/manager/exception/workspace/WorkspaceHasNoDevelopmentStageException.java
@@ -2,7 +2,6 @@ package com.itextpdf.dito.manager.exception.workspace;
 
 public class WorkspaceHasNoDevelopmentStageException extends RuntimeException {
     private static final long serialVersionUID = 1L;
-
     private static final String message = "Workspace has no development stage at all.";
 
     public WorkspaceHasNoDevelopmentStageException() {
