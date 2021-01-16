05b23bcf84c0639ff7b4f4535ee3271c62f32d13
Yaroslaw Maievskij <y.maievskyi@andersenlab.com>
2021-01-16 13:38:56 +0200

    Implement business logic: create new data sample based on json
    DTM-1003

--- src/main/java/com/itextpdf/dito/manager/repository/datasample/DataSampleRepository.java
+++ src/main/java/com/itextpdf/dito/manager/repository/datasample/DataSampleRepository.java
@@ -2,7 +2,9 @@ package com.itextpdf.dito.manager.repository.datasample;
 
 import com.itextpdf.dito.manager.entity.datasample.DataSampleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DataSampleRepository extends JpaRepository<DataSampleEntity, Long> {
   
 }
