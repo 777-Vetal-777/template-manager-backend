97050ac565d57beba6a75983f3f5a82c338cdefa
Ivan Semenov <i.semenov@andersenlab.com>
2021-01-18 16:33:24 +0400

    Add and fix tests
    DTM-741

--- src/test/java/com/itextpdf/dito/manager/integration/crud/permissions/DataCollectionPermissionsFlowIntegrationTest.java
+++ src/test/java/com/itextpdf/dito/manager/integration/crud/permissions/DataCollectionPermissionsFlowIntegrationTest.java
@@ -78,7 +78,6 @@ public class DataCollectionPermissionsFlowIntegrationTest extends AbstractIntegr
     }
 
     @Test
-    @Disabled
     public void test_dataCollectionPermissions() throws Exception {
         final String encodedDataCollectionName = encodeStringToBase64(dataCollectionName);
 
@@ -92,25 +91,27 @@ public class DataCollectionPermissionsFlowIntegrationTest extends AbstractIntegr
 
         //Get permission for data collection
         mockMvc.perform(get(DataCollectionController.BASE_NAME + DataCollectionController.DATA_COLLECTION_APPLIED_ROLES_ENDPOINT_WITH_DATA_COLLECTION_PATH_VARIABLE, encodedDataCollectionName)
-                .param("roleName", roleName))
+                .param("name", roleName))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content", hasSize(1)));
-                /*.andExpect(jsonPath("$.content[0].e7_US48_CREATE_NEW_VERSION_OF_DATA_SAMPLE", is("true")))
-                .andExpect(jsonPath("$.content[0].e6_US38_DELETE_DATA_COLLECTION", is("true")))
-                .andExpect(jsonPath("$.content[0].e7_US50_DELETE_DATA_SAMPLE", is("true")))
-                .andExpect(jsonPath("$.content[0].e7_US47_EDIT_SAMPLE_METADATA", is("true")))
-                .andExpect(jsonPath("$.content[0].e6_US34_EDIT_DATA_COLLECTION_METADATA", is("true")))
-                .andExpect(jsonPath("$.content[0].e6_US35_CREATE_A_NEW_VERSION_OF_DATA_COLLECTION_USING_JSON", is("true")))
-                .andExpect(jsonPath("$.content[0].e7_US44_CREATE_NEW_DATA_SAMPLE_BASED_ON_JSON_FILE", is("true")))
-                .andExpect(jsonPath("$.content[0].roleName", is("role-for-permissions-test")));*/
 
-        //Sort and search permission for data collection
+        //Sort and filter permission for data collection
         for (String field : DataCollectionPermissionsRepository.SUPPORTED_SORT_PERMISSION_FIELDS) {
             mockMvc.perform(get(DataCollectionController.BASE_NAME + DataCollectionController.DATA_COLLECTION_APPLIED_ROLES_ENDPOINT_WITH_DATA_COLLECTION_PATH_VARIABLE, encodedDataCollectionName)
                     .param("sort", field)
-                    .param("roleName", roleName))
+                    .param("name", roleName))
                     .andExpect(status().isOk());
         }
+
+        //Search permission for data collection
+        for (String field : DataCollectionPermissionsRepository.SUPPORTED_SORT_PERMISSION_FIELDS) {
+            mockMvc.perform(get(DataCollectionController.BASE_NAME + DataCollectionController.DATA_COLLECTION_APPLIED_ROLES_ENDPOINT_WITH_DATA_COLLECTION_PATH_VARIABLE, encodedDataCollectionName)
+                    .param("sort", field)
+                    .param("search", field)
+                    .param("name", roleName))
+                    .andExpect(status().isOk());
+        }
+
 
         //Delete data collection permission
         mockMvc.perform(delete(DataCollectionController.BASE_NAME + DataCollectionController.DATA_COLLECTION_APPLIED_ROLES_ENDPOINT_WITH_DATA_COLLECTION_AND_ROLE_PATH_VARIABLES, encodedDataCollectionName, encodeStringToBase64(roleName)))
