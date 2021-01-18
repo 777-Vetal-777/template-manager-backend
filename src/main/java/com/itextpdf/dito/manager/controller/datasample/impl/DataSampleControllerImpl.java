3b0eca9cbf08f7e2b82b5ea0a48ecf2a3da3e40e
Ivan Semenov <i.semenov@andersenlab.com>
2021-01-18 13:20:31 +0400

    Add data samples table
    DTM-997, DTM-998

--- src/main/java/com/itextpdf/dito/manager/controller/datasample/impl/DataSampleControllerImpl.java
+++ src/main/java/com/itextpdf/dito/manager/controller/datasample/impl/DataSampleControllerImpl.java
@@ -1,23 +1,49 @@
 package com.itextpdf.dito.manager.controller.datasample.impl;
 
+import com.itextpdf.dito.manager.component.mapper.datasample.DataSampleMapper;
 import com.itextpdf.dito.manager.controller.AbstractController;
 import com.itextpdf.dito.manager.controller.datasample.DataSampleController;
 import com.itextpdf.dito.manager.dto.datacollection.DataCollectionDTO;
+import com.itextpdf.dito.manager.dto.datasample.DataSampleDTO;
 import com.itextpdf.dito.manager.dto.datasample.create.DataSampleCreateRequestDTO;
 
 import java.security.Principal;
 
+import com.itextpdf.dito.manager.filter.datasample.DataSampleFilter;
+import com.itextpdf.dito.manager.service.datasample.DataSampleService;
 import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DataSampleControllerImpl extends AbstractController implements DataSampleController {
 
+    private final DataSampleService dataSampleService;
+    private final DataSampleMapper dataSampleMapper;
+
+    public DataSampleControllerImpl(final DataSampleService dataSampleService,
+                                    final DataSampleMapper dataSampleMapper) {
+        this.dataSampleService = dataSampleService;
+        this.dataSampleMapper = dataSampleMapper;
+    }
+
+
     @Override
-    public ResponseEntity<DataCollectionDTO> create(final DataSampleCreateRequestDTO templateCreateRequestDTO,
-                                                    final Principal principal) {
+    public ResponseEntity<DataSampleDTO> create(final DataSampleCreateRequestDTO templateCreateRequestDTO,
+                                                final Principal principal) {
         throw new NotImplementedException("Not realized yet");
     }
 
+    @Override
+    public ResponseEntity<Page<DataSampleDTO>> list(final Pageable pageable, final DataSampleFilter filter, final String searchParam) {
+        return new ResponseEntity<>(dataSampleMapper.map(dataSampleService.list(pageable, filter, searchParam)), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<DataSampleDTO> get(final String dataSampleName) {
+        return new ResponseEntity<>(dataSampleMapper.map(dataSampleService.get(dataSampleName)), HttpStatus.OK);
+    }
 }
--- src/main/java/com/itextpdf/dito/manager/repository/datasample/DataSampleRepository.java
+++ src/main/java/com/itextpdf/dito/manager/repository/datasample/DataSampleRepository.java
@@ -1,10 +1,55 @@
 package com.itextpdf.dito.manager.repository.datasample;
 
+import com.itextpdf.dito.manager.entity.TemplateTypeEnum;
 import com.itextpdf.dito.manager.entity.datasample.DataSampleEntity;
+import com.itextpdf.dito.manager.entity.template.TemplateEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Temporal;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DataSampleRepository extends JpaRepository<DataSampleEntity, Long> {
-  
+    List<String> SUPPORTED_SORT_FIELDS = List.of("name", "modifiedBy", "modifiedOn", "comment", "setAsDefault");
+
+    String DATA_SAMPLE_TABLE_SELECT_CLAUSE = "select ds from DataSampleEntity ds where ";
+
+    String DATA_SAMPLE_TABLE_FILTER_CONDITION = "(:name='' or LOWER(ds.name) like CONCAT('%',:name,'%')) "
+            + "and (:modifiedBy='' or LOWER(CONCAT(ds.author.firstName, ' ', ds.author.lastName)) like CONCAT('%',:modifiedBy,'%')) "
+            + "and (cast(:startDate as date) is null or ds.modifiedOn between cast(:startDate as date) and cast(:endDate as date)) "
+            + "and (:comment='' or LOWER(ds.comment) like CONCAT('%',:comment,'%')) ";
+
+    String DATA_SAMPLE_TABLE_SEARCH_CONDITION = " and (CAST(ds.modifiedOn as string) like CONCAT('%',:search,'%') "
+            + "or LOWER(ds.comment) like CONCAT('%',:search,'%') "
+            + "or LOWER(ds.name) like CONCAT('%',:search,'%') "
+            + "or LOWER(CONCAT(ds.author.firstName, ' ', ds.author.lastName)) like LOWER(CONCAT('%',:search,'%')))";
+
+    @Query(value = DATA_SAMPLE_TABLE_SELECT_CLAUSE + DATA_SAMPLE_TABLE_FILTER_CONDITION)
+    Page<DataSampleEntity> filter(Pageable pageable,
+                                  @Param("name") @Nullable String name,
+                                  @Param("modifiedBy") @Nullable String modifiedBy,
+                                  @Param("startDate") @Nullable @Temporal Date modifiedOnStartDate,
+                                  @Param("endDate") @Nullable @Temporal Date modifiedOnEndDate,
+                                  //TODO add setAsDefault param
+                                  @Param("comment") @Nullable String comment);
+
+    @Query(value = DATA_SAMPLE_TABLE_SELECT_CLAUSE + DATA_SAMPLE_TABLE_FILTER_CONDITION + DATA_SAMPLE_TABLE_SEARCH_CONDITION)
+    Page<DataSampleEntity> search(Pageable pageable,
+                                  @Param("name") @Nullable String name,
+                                  @Param("modifiedBy") @Nullable String modifiedBy,
+                                  @Param("startDate") @Nullable @Temporal Date modifiedOnStartDate,
+                                  @Param("endDate") @Nullable @Temporal Date modifiedOnEndDate,
+                                  @Param("comment") @Nullable String comment,
+                                  //TODO add setAsDefault param
+                                  @Param("search") @Nullable String search);
+
+    Optional<DataSampleEntity> findByName(String name);
 }
