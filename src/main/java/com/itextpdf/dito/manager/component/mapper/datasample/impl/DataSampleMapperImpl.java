d4e5460b8929ef8e8257dd2515ebd68210da9754
Denys Kovbii <d.kovbii@andersenlab.com>
2021-01-20 13:44:51 +0200

    DataSamples endpoints with buiseness logic added
    Added setAsDefault endpoint, checks for the unique data samle names, Delete data samples contracts, Filter is modified
    
    DTM-849

--- src/main/java/com/itextpdf/dito/manager/component/mapper/datasample/impl/DataSampleMapperImpl.java
+++ src/main/java/com/itextpdf/dito/manager/component/mapper/datasample/impl/DataSampleMapperImpl.java
@@ -13,16 +13,7 @@ public class DataSampleMapperImpl implements DataSampleMapper {
 
 	private final JsonKeyComparator jsonKeyComparator;
 	
-<<<<<<< HEAD
 	public DataSampleMapperImpl(final JsonKeyComparator jsonKeyComparator) {
-=======
-	
-<<<<<<< HEAD
-	public DataSampleMapperImpl(JsonKeyComparator jsonKeyComparator) {
->>>>>>> DataSamples endpoints added
-=======
-	public DataSampleMapperImpl(final JsonKeyComparator jsonKeyComparator) {
->>>>>>> DataSamples endpoints added
 		this.jsonKeyComparator = jsonKeyComparator;
 	}
 
@@ -38,11 +29,7 @@ public class DataSampleMapperImpl implements DataSampleMapper {
         dto.setAuthorLastName(entity.getAuthor().getLastName());
         dto.setComment(entity.getComment());
         dto.setFileName(entity.getFileName());
-<<<<<<< HEAD
         dto.setIsDefault(entity.getIsDefault());
-=======
-        dto.setSetAsDefault(entity.getSetAsDefault());
->>>>>>> DataSamples endpoints added
 		dto.setIsActual(jsonKeyComparator.checkJsonKeysEquals(new String(entity.getData()),
 				new String(entity.getDataCollection().getLatestVersion().getData())));
 		 return dto;
