ba6a51fd2ad912286b5015b230a08a8bd773dca5
VADZIM SAROKIN <v.sorokin@andersenlab.com>
2021-01-25 11:09:35 +0300

    Editor api dependency is ON
    DTM-1106

--- src/main/java/com/itextpdf/dito/manager/integration/editor/mapper/template/impl/TemplateDescriptorMapperImpl.java
+++ src/main/java/com/itextpdf/dito/manager/integration/editor/mapper/template/impl/TemplateDescriptorMapperImpl.java
@@ -1,7 +1,7 @@
 package com.itextpdf.dito.manager.integration.editor.mapper.template.impl;
 
-//import com.itextpdf.dito.editor.server.common.core.descriptor.TemplateDescriptor;
-//import com.itextpdf.dito.editor.server.common.core.descriptor.TemplateDescriptor.OutputTemplateDescriptor;
+import com.itextpdf.dito.editor.server.common.core.descriptor.TemplateDescriptor;
+import com.itextpdf.dito.editor.server.common.core.descriptor.TemplateDescriptor.OutputTemplateDescriptor;
 import com.itextpdf.dito.manager.entity.template.TemplateEntity;
 import com.itextpdf.dito.manager.integration.editor.mapper.template.TemplateDescriptorMapper;
 
@@ -12,22 +12,22 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TemplateDescriptorMapperImpl implements TemplateDescriptorMapper {
-//    @Override
-//    public TemplateDescriptor map(final TemplateEntity templateEntity) {
-//        final TemplateDescriptor result;
-//        final String templateName = templateEntity.getName();
-//        result = new OutputTemplateDescriptor(encodeToBase64(templateName));
-//        result.setDisplayName(templateName);
-//        final String dataCollectionName = templateEntity.getLatestFile().getDataCollectionFile().getDataCollection()
-//                .getName();
-//        result.setDataCollectionId(encodeToBase64(dataCollectionName));
-//        return result;
-//    }
-//
-//    @Override
-//    public List<TemplateDescriptor> map(List<TemplateEntity> entities) {
-//        return entities.stream().map(this::map).collect(Collectors.toList());
-//    }
+    @Override
+    public TemplateDescriptor map(final TemplateEntity templateEntity) {
+        final TemplateDescriptor result;
+        final String templateName = templateEntity.getName();
+        result = new OutputTemplateDescriptor(encodeToBase64(templateName));
+        result.setDisplayName(templateName);
+        final String dataCollectionName = templateEntity.getLatestFile().getDataCollectionFile().getDataCollection()
+                .getName();
+        result.setDataCollectionId(encodeToBase64(dataCollectionName));
+        return result;
+    }
+
+    @Override
+    public List<TemplateDescriptor> map(List<TemplateEntity> entities) {
+        return entities.stream().map(this::map).collect(Collectors.toList());
+    }
 
     private String encodeToBase64(final String value) {
         return new String(Base64.getUrlEncoder().encode(value.getBytes()));
