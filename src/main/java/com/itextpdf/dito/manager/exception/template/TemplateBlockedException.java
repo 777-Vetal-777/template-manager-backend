13c3090da287a3a258f246363c1e49a1034c369b
Ivan Semenov <i.semenov@andersenlab.com>
2021-01-25 19:55:19 +0400

    Move scheduler values to application properties
    DTM-1250

--- src/main/java/com/itextpdf/dito/manager/exception/template/TemplateBlockedException.java
+++
@@ -1,9 +0,0 @@
-package com.itextpdf.dito.manager.exception.template;
-
-public class TemplateBlockedException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public TemplateBlockedException(final String message) {
-        super(message);
-    }
-}
