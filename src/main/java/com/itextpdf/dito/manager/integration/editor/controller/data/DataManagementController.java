634563ffdd98c2869d6a346fba93642650b2c7b0
VADZIM SAROKIN <v.sorokin@andersenlab.com>
2021-01-26 09:59:07 +0300

    API contract updates for data samples
    DTM-1106

--- src/main/java/com/itextpdf/dito/manager/integration/editor/controller/data/DataManagementController.java
+++ src/main/java/com/itextpdf/dito/manager/integration/editor/controller/data/DataManagementController.java
@@ -1,12 +1,15 @@
 package com.itextpdf.dito.manager.integration.editor.controller.data;
 
 import com.itextpdf.dito.editor.server.common.core.descriptor.DataSampleDescriptor;
+
 import java.io.InputStream;
 import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 
 public interface DataManagementController {
     String CREATE_DATA_SAMPLE_URL = "/data";
@@ -15,20 +18,21 @@ public interface DataManagementController {
     String COLLECTION_DATA_SAMPLES_URL = "/collection/{collection-id}";
 
     @GetMapping(DATA_SAMPLE_DESCRIPTOR_URL)
-    DataSampleDescriptor getDataSampleById(String dataSampleId);
+    DataSampleDescriptor getDataSampleById(@PathVariable("sample-id") String dataSampleId);
 
     @GetMapping(DATA_SAMPLE_URL)
-    InputStream fetchDataSampleById(String dataSampleId);
+    InputStream fetchDataSampleById(@PathVariable("sample-id") String dataSampleId);
 
     @PutMapping(DATA_SAMPLE_URL)
-    DataSampleDescriptor createOrUpdate(String dataSampleId, DataSampleDescriptor descriptor, InputStream data);
+    DataSampleDescriptor createOrUpdate(@PathVariable("sample-id") String dataSampleId,
+            @RequestPart DataSampleDescriptor descriptor, @RequestPart InputStream data);
 
     @PostMapping(CREATE_DATA_SAMPLE_URL)
-    DataSampleDescriptor add(DataSampleDescriptor descriptor, InputStream data);
+    DataSampleDescriptor add(@RequestPart DataSampleDescriptor descriptor, @RequestPart InputStream data);
 
     @DeleteMapping(DATA_SAMPLE_URL)
-    DataSampleDescriptor deleteDataSampleById(String dataSampleId);
+    DataSampleDescriptor deleteDataSampleById(@PathVariable("sample-id") String dataSampleId);
 
     @GetMapping(COLLECTION_DATA_SAMPLES_URL)
-    List<DataSampleDescriptor> getDataSamplesByCollectionId(String collectionId);
+    List<DataSampleDescriptor> getDataSamplesByCollectionId(@PathVariable("collection-id") String collectionId);
 }
