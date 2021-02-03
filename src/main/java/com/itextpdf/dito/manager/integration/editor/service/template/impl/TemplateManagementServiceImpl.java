a0763be7bf7ac8cb9fe6ea6d33e6b07f9e597b72
VADZIM SAROKIN <v.sorokin@andersenlab.com>
2021-02-03 11:33:54 +0300

    Adding api/resources endpoint
    DTM-1677

--- src/main/java/com/itextpdf/dito/manager/integration/editor/service/template/impl/TemplateManagementServiceImpl.java
+++ src/main/java/com/itextpdf/dito/manager/integration/editor/service/template/impl/TemplateManagementServiceImpl.java
@@ -4,7 +4,14 @@ import com.itextpdf.dito.manager.entity.TemplateTypeEnum;
 import com.itextpdf.dito.manager.entity.template.TemplateEntity;
 import com.itextpdf.dito.manager.integration.editor.service.template.TemplateManagementService;
 import com.itextpdf.dito.manager.service.template.TemplateService;
+import com.itextpdf.dito.sdk.core.dependency.api.TemplateDependency;
+import com.itextpdf.dito.sdk.core.dependency.retriever.template.DefaultTemplateDependenciesRetriever;
+import com.itextpdf.dito.sdk.core.dependency.retriever.template.TemplateDependenciesRetriever;
+import com.itextpdf.dito.sdk.core.preprocess.asset.TemplateAssetRetriever;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -28,7 +35,8 @@ public class TemplateManagementServiceImpl implements TemplateManagementService 
     }
 
     @Override
-    public TemplateEntity createNewVersion(final String name, final byte[] data, final String email, final String newName) {
+    public TemplateEntity createNewVersion(final String name, final byte[] data, final String email,
+            final String newName) {
         return templateService.createNewVersion(name, data, email, null, newName);
     }
 
@@ -41,4 +49,38 @@ public class TemplateManagementServiceImpl implements TemplateManagementService 
     public TemplateEntity delete(final String templateName) {
         return templateService.delete(templateName);
     }
+
+    private void provideConsistency(final byte[] data) {
+        final TemplateDependenciesRetriever retriever = new DefaultTemplateDependenciesRetriever(
+                templateAssetRetriever, resourceAssetRetriever);
+        try {
+            final List<TemplateDependency> dependencies = retriever.getDependencies(new ByteArrayInputStream(data));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static TemplateAssetRetriever templateAssetRetriever = new TemplateAssetRetriever() {
+        @Override
+        public InputStream getResourceAsStream(String s) throws IOException {
+            return null;
+        }
+
+        @Override
+        public boolean isUriSupported(String s) {
+            return false;
+        }
+    };
+
+    private static TemplateAssetRetriever resourceAssetRetriever = new TemplateAssetRetriever() {
+        @Override
+        public InputStream getResourceAsStream(String s) throws IOException {
+            return null;
+        }
+
+        @Override
+        public boolean isUriSupported(String s) {
+            return false;
+        }
+    };
 }
