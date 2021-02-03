cfda0ec0003151781c588351b0acddcccbc1c027
Denys Kovbii <d.kovbii@andersenlab.com>
2021-02-03 08:50:54 +0200

    Web API for lisence
    upload and get information about lisence API contracts.DTM-1417, DTM-1411
    
    DTM-1416

--- src/main/java/com/itextpdf/dito/manager/controller/workspace/WorkspaceController.java
+++ src/main/java/com/itextpdf/dito/manager/controller/workspace/WorkspaceController.java
@@ -1,11 +1,16 @@
 package com.itextpdf.dito.manager.controller.workspace;
 
 import com.itextpdf.dito.manager.config.OpenApiConfig;
+import com.itextpdf.dito.manager.dto.datacollection.DataCollectionDTO;
+import com.itextpdf.dito.manager.dto.datacollection.DataCollectionType;
+import com.itextpdf.dito.manager.dto.license.LicenseDTO;
 import com.itextpdf.dito.manager.dto.promotionpath.PromotionPathDTO;
 import com.itextpdf.dito.manager.dto.workspace.WorkspaceDTO;
 import com.itextpdf.dito.manager.dto.workspace.create.WorkspaceCreateRequestDTO;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterStyle;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -17,6 +22,8 @@ import java.security.Principal;
 import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +32,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping(WorkspaceController.BASE_NAME)
 @Tag(name = "workspace", description = "workspace API")
@@ -36,6 +45,7 @@ public interface WorkspaceController {
     String WORKSPACE_ENDPOINT_WITH_PATH_VARIABLE = "/{" + WORKSPACE_PATH_VARIABLE + "}";
     String WORKSPACE_PROMOTION_PATH_ENDPOINT = WORKSPACE_ENDPOINT_WITH_PATH_VARIABLE + "/promotion-path";
     String WORKSPACE_STAGES_ENDPOINT = WORKSPACE_ENDPOINT_WITH_PATH_VARIABLE + "/stages";
+    String WORKSPACE_LICENSE_ENDPOINT = WORKSPACE_ENDPOINT_WITH_PATH_VARIABLE + "/license";
 
     @PostMapping
     @PreAuthorize("hasAnyAuthority('E4_US18_WORKSPACE_DEFAULT_SETTINGS', 'E4_US23_WORKSPACE_SET_UP_WIZARD')")
@@ -82,4 +92,21 @@ public interface WorkspaceController {
     @Operation(summary = "List of stages for workspace", description = "Retrieve list of stage names for workspace",
             security = @SecurityRequirement(name = OpenApiConfig.BEARER_AUTH_SECURITY_SCHEME_NAME))
     ResponseEntity<List<String>> getStageNames(@PathVariable(WORKSPACE_PATH_VARIABLE) String workspaceName);
+    
+    @PostMapping(path = WORKSPACE_LICENSE_ENDPOINT, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PreAuthorize("hasAuthority('E1_US1_LOG_IN_TO_THE_SYSTEM')")
+    @Operation(summary = "Upload license",
+            security = @SecurityRequirement(name = OpenApiConfig.BEARER_AUTH_SECURITY_SCHEME_NAME))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully uploaded", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Invalid license", content = @Content),
+    })
+    ResponseEntity<Void> uploadLisence(@Parameter(description = "license XML file", required = true, style = ParameterStyle.FORM) @RequestPart("license") MultipartFile multipartFile, Principal principal);
+
+    @GetMapping(path = WORKSPACE_LICENSE_ENDPOINT, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PreAuthorize("hasAuthority('E1_US1_LOG_IN_TO_THE_SYSTEM')")
+    @Operation(summary = "Get license information",
+            security = @SecurityRequirement(name = OpenApiConfig.BEARER_AUTH_SECURITY_SCHEME_NAME))
+    ResponseEntity<LicenseDTO> getLisence(@PathVariable(WORKSPACE_PATH_VARIABLE) String workspaceName, Principal principal);
+
 }
