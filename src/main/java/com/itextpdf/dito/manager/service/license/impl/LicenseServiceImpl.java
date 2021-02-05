37d02281f8f5247f57d3d0905254e1c37dc07c08
Denys Kovbii <d.kovbii@andersenlab.com>
2021-02-05 15:12:46 +0200

    License modifications for re-new file
    modifications for re-new file license
    
    DTM-1423

--- src/main/java/com/itextpdf/dito/manager/service/license/impl/LicenseServiceImpl.java
+++ src/main/java/com/itextpdf/dito/manager/service/license/impl/LicenseServiceImpl.java
@@ -1,6 +1,9 @@
 package com.itextpdf.dito.manager.service.license.impl;
 
 import java.io.ByteArrayInputStream;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -8,9 +11,9 @@ import org.springframework.stereotype.Component;
 import com.itextpdf.dito.manager.entity.LicenseEntity;
 import com.itextpdf.dito.manager.entity.WorkspaceEntity;
 import com.itextpdf.dito.manager.exception.license.InvalidLicenseException;
-import com.itextpdf.dito.manager.exception.license.LicenseAlreadyExistsException;
 import com.itextpdf.dito.manager.exception.license.LicenseNotFoundException;
 import com.itextpdf.dito.manager.repository.license.LicenseRepository;
+import com.itextpdf.dito.manager.repository.workspace.WorkspaceRepository;
 import com.itextpdf.dito.manager.service.license.LicenseService;
 import com.itextpdf.dito.sdk.license.DitoLicense;
 import com.itextpdf.dito.sdk.license.DitoLicenseException;
@@ -18,22 +21,29 @@ import com.itextpdf.dito.sdk.license.DitoLicenseException;
 @Component
 public class LicenseServiceImpl implements LicenseService {
 	private LicenseRepository licenseRepository;
+	private WorkspaceRepository workspaceRepository;
 
 	@Autowired
-	public LicenseServiceImpl(final LicenseRepository licenseRepository) {
+	public LicenseServiceImpl(final LicenseRepository licenseRepository,
+			final WorkspaceRepository workspaceRepository) {
 		this.licenseRepository = licenseRepository;
+		this.workspaceRepository = workspaceRepository;
 	}
 
 	@Override
-	public LicenseEntity uploadLicense(final WorkspaceEntity workspaceEntity, final byte[] data, final String fileName) {
-		if (licenseRepository.findByWorkspace(workspaceEntity).isPresent()) {
-			throw new LicenseAlreadyExistsException(workspaceEntity.getName());
-		}
-
+	@Transactional
+	public LicenseEntity uploadLicense(final WorkspaceEntity workspaceEntity, final byte[] data,
+			final String fileName) {
 		try {
 			DitoLicense.parseLicense(new ByteArrayInputStream(data));
 		} catch (DitoLicenseException e) {
 			throw new InvalidLicenseException();
+		}
+		if (workspaceEntity.getLicenseEntity() != null) {
+			final LicenseEntity entityToDelete = workspaceEntity.getLicenseEntity();
+			workspaceEntity.setLicenseEntity(null);
+			workspaceRepository.save(workspaceEntity);
+			licenseRepository.delete(entityToDelete);
 		}
 		final LicenseEntity entity = new LicenseEntity();
 		entity.setData(data);
