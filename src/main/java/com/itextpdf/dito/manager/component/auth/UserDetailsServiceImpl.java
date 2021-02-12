f3fe4ccd2f1c946aacce947cb34c6be1df3ae739
Ivan Semenov <i.semenov@andersenlab.com>
2021-02-12 17:48:44 +0400

    Move admin credentials to env variable
    DTM-1736

--- src/main/java/com/itextpdf/dito/manager/component/auth/UserDetailsServiceImpl.java
+++ src/main/java/com/itextpdf/dito/manager/component/auth/UserDetailsServiceImpl.java
@@ -1,14 +1,30 @@
 package com.itextpdf.dito.manager.component.auth;
 
+import com.itextpdf.dito.manager.entity.UserEntity;
+import com.itextpdf.dito.manager.exception.user.UserNotFoundException;
 import com.itextpdf.dito.manager.repository.user.UserRepository;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private static final Logger LOG = LogManager.getLogger(UserDetailsServiceImpl.class);
+
+    @Value("${security.credentials.admin-email:#{null}}")
+    private String defaultAdminEmail;
+    @Value("${security.credentials.admin-password:#{null}}")
+    private String defaultAdminPassword;
+
     private final UserRepository userRepository;
 
     public UserDetailsServiceImpl(final UserRepository userRepository) {
@@ -22,6 +38,19 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                         () -> new UsernameNotFoundException(
                                 new StringBuilder("No user with email ").append(username).append(" was found")
                                         .toString()));
+    }
+
+    @PostConstruct
+    public void updateAdminProperties() {
+        final Long defaultAdminId = 1L;
+        if (!StringUtils.isEmpty(defaultAdminEmail) && !StringUtils.isEmpty(defaultAdminPassword)) {
+            UserEntity userEntity = userRepository.findById(defaultAdminId).orElseThrow(() -> new UserNotFoundException("Admin user not found"));
+            userEntity.setEmail(defaultAdminEmail);
+            userEntity.setPassword(defaultAdminPassword);
+            userRepository.save(userEntity);
+        } else {
+            LOG.warn("No email and password for default admin user were set. Default credentials will be used");
+        }
     }
 }
 
