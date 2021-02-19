2027fe4caa90c15438094f6bb97744525c183af4
Sergey Nemets <s.nemets@andersenlab.com>
2021-02-19 14:09:32 +0300

    About page
    add BE unit test
    
    DTM-1884

---
+++ src/test/java/com/itextpdf/dito/manager/integration/crud/ActuatorFlowIntegrationTest.java
@@ -0,0 +1,49 @@
+package com.itextpdf.dito.manager.integration.crud;
+
+import com.itextpdf.dito.manager.integration.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ActuatorFlowIntegrationTest extends AbstractIntegrationTest {
+
+    public static final String ACTUATOR_PATH = "/actuator";
+    public static final String ACTUATOR_INFO_PATH = ACTUATOR_PATH + "/info";
+    public static final String ACTUATOR_HEALTH_PATH = ACTUATOR_PATH + "/health";
+    public static final String ACTUATOR_ENV_PATH = ACTUATOR_PATH + "/env";
+
+    @Test
+    void testInfoEndPoint() throws Exception {
+        mockMvc.perform(get(ACTUATOR_INFO_PATH))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("build.version").isNotEmpty())
+                .andExpect(jsonPath("build.name").isNotEmpty())
+                .andExpect(jsonPath("build.time").isNotEmpty())
+                .andExpect(jsonPath("licenses").isArray())
+                .andExpect(jsonPath("dependencies").isArray());
+    }
+
+    @Test
+    void testHealthEndPoint() throws Exception {
+        mockMvc.perform(get(ACTUATOR_HEALTH_PATH))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("status").isNotEmpty())
+                .andExpect(jsonPath("components.db").isNotEmpty())
+                .andExpect(jsonPath("components.diskSpace").isNotEmpty())
+                .andExpect(jsonPath("components.ping").isNotEmpty())
+                .andExpect(jsonPath("components.ping.status").value("UP"));
+    }
+
+    @Test
+    void testEnvEndPoint() throws Exception {
+        mockMvc.perform(get(ActuatorFlowIntegrationTest.ACTUATOR_ENV_PATH))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("activeProfiles", containsInAnyOrder("integration-test")))
+                .andExpect(jsonPath("propertySources").isArray());
+    }
+
+
+}
