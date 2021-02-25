14e0d6293cb17549d511f45b7af07c9ebbd57d15
Yaroslaw <y.maievskyi@andersenlab.com>
2021-02-25 12:04:41 +0200

    Code refactoring
    DTM-1906

--- src/main/java/com/itextpdf/dito/manager/repository/resource/ResourcePermissionRepository.java
+++ src/main/java/com/itextpdf/dito/manager/repository/resource/ResourcePermissionRepository.java
@@ -17,7 +17,15 @@ public interface ResourcePermissionRepository extends JpaRepository<TemplateEnti
             "E8_US66_DELETE_RESOURCE_IMAGE",
             "E8_US62_CREATE_NEW_VERSION_OF_RESOURCE_IMAGE",
             "E8_US55_EDIT_RESOURCE_METADATA_IMAGE",
-            "E8_US65_ROLL_BACK_OF_THE_RESOURCE_IMAGE");
+            "E8_US65_ROLL_BACK_OF_THE_RESOURCE_IMAGE",
+            "E8_US66_1_DELETE_RESOURCE_FONT",
+            "E8_US62_1_CREATE_NEW_VERSION_OF_RESOURCE_FONT",
+            "E8_US58_EDIT_RESOURCE_METADATA_FONT",
+            "E8_US65_1_ROLL_BACK_OF_THE_RESOURCE_FONT",
+            "E8_US66_2_DELETE_RESOURCE_STYLESHEET",
+            "E8_US63_CREATE_NEW_VERSION_OF_RESOURCE_STYLESHEET",
+            "E8_US61_EDIT_RESOURCE_METADATA_STYLESHEET",
+            "E8_US65_2_ROLL_BACK_OF_THE_RESOURCE_STYLESHEET");
 
 
     String SELECT_FIELDS_CLAUSE = "select name, type, resourceType, lower(name) as lower_name, " +
