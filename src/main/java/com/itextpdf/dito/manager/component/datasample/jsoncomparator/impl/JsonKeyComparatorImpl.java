ab84f0d3e6ede0d3114efb2ba7379f6aed806b84
Denys Kovbii <d.kovbii@andersenlab.com>
2021-03-02 10:35:47 +0200

    Removing dependency org.json
    Changing validation flow for datasamples
    
    DTM-2241

--- src/main/java/com/itextpdf/dito/manager/component/datasample/jsoncomparator/impl/JsonKeyComparatorImpl.java
+++ src/main/java/com/itextpdf/dito/manager/component/datasample/jsoncomparator/impl/JsonKeyComparatorImpl.java
@@ -1,36 +1,51 @@
 package com.itextpdf.dito.manager.component.datasample.jsoncomparator.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itextpdf.dito.manager.component.datasample.jsoncomparator.JsonKeyComparator;
+import com.itextpdf.dito.manager.component.mapper.permission.impl.PermissionMapperImpl;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Set;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JsonKeyComparatorImpl implements JsonKeyComparator {
-
+	
+	private static final Logger log = LogManager.getLogger(JsonKeyComparatorImpl.class);
+	private ObjectMapper mapper;
+	
+	public JsonKeyComparatorImpl(final ObjectMapper mapper) {
+		this.mapper = mapper;
+	}
+	
 	@Override
 	public boolean checkJsonKeysEquals(final String json1, final String json2) {
 		boolean result;
 		try {
 			result = checkLastDataCollectionEqualsSample(json1, json2);
-		} catch (JSONException e) {
+		} catch (JsonProcessingException  e) {
+			log.info("Checking datasample to datacollection structure consistency failed. Error message: {}", e.getMessage());
 			result = false;
 		}
 		return result;
 	}
 
-    private boolean checkLastDataCollectionEqualsSample(final String json1, final String json2) {
-        final List<String> keys1 = Arrays.asList(getJsonKeys(json1));
-        final List<String> keys2 = Arrays.asList(getJsonKeys(json2));
-        return (keys2.containsAll(keys1) && keys1.containsAll(keys2));
+    private boolean checkLastDataCollectionEqualsSample(final String json1, final String json2) throws JsonProcessingException {
+        final Set<String> keys1 = getJsonKeys(json1);
+        final Set<String> keys2 =  getJsonKeys(json2);
+        final boolean sizeCheck = keys1.size() == keys2.size();
+        keys1.removeAll(keys2);
+        return sizeCheck && keys1.isEmpty();
     }
 
-    private String[] getJsonKeys(final String json) {
-        final JSONObject object = new JSONObject(json);
-        return JSONObject.getNames(object);
+    private Set<String> getJsonKeys(final String json) throws JsonProcessingException {
+    	return mapper.readValue(json, new TypeReference<HashMap<String,Object>>() {}).keySet();
     }
 }
