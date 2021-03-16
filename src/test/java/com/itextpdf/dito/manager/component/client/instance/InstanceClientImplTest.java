cefceb29f85c5cd6a87d5b017c21213f13671639
Yaroslav_Maievskiy <y.maievskyi@andersenlab.com>
2021-03-16 15:43:12 +0200

    Test writed: ping instance
    DTM-2504

---
+++ src/test/java/com/itextpdf/dito/manager/component/client/instance/InstanceClientImplTest.java
@@ -0,0 +1,77 @@
+package com.itextpdf.dito.manager.component.client.instance;
+
+import com.itextpdf.dito.manager.controller.instance.InstanceController;
+import com.itextpdf.dito.manager.dto.instance.register.InstanceRegisterResponseDTO;
+import com.itextpdf.dito.manager.repository.instance.InstanceRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
+import org.mockito.ArgumentMatchers;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithUserDetails("admin@email.com")
+@ActiveProfiles("integration-test")
+@ExtendWith(MockitoExtension.class)
+public class InstanceClientImplTest {
+    private final static String INSTANCE_NAME = "INSTANCE_NAME";
+    private final static String SOCKET_EXAMPLE = "http://asd10.10.15.15:9092";
+    private final static String ENCODED_SOCKET_EXAMPLE = Base64.getEncoder().encodeToString(SOCKET_EXAMPLE.getBytes(StandardCharsets.UTF_8));
+
+    @Autowired
+    private InstanceRepository instanceRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean(answer = Answers.RETURNS_DEEP_STUBS)
+    private WebClient webClientMock;
+
+    @SuppressWarnings("rawtypes")
+    @MockBean
+    private WebClient.RequestHeadersSpec requestHeadersSpecMock;
+
+    @SuppressWarnings("rawtypes")
+    @MockBean
+    private WebClient.RequestHeadersUriSpec requestHeadersUriSpecMock;
+
+    @MockBean
+    private WebClient.ResponseSpec responseSpecMock;
+
+    @AfterEach
+    public void cleanUp() {
+        instanceRepository.deleteAll();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldSendPingInstance() throws Exception {
+        final InstanceRegisterResponseDTO post = new InstanceRegisterResponseDTO();
+        post.setToken("randToken");
+
+        when(webClientMock.get()).thenReturn(requestHeadersUriSpecMock);
+        when(requestHeadersUriSpecMock.uri(anyString())).thenReturn(requestHeadersSpecMock);
+        when(requestHeadersSpecMock.retrieve()).thenReturn(responseSpecMock);
+        when(responseSpecMock.bodyToMono(ArgumentMatchers.<Class<InstanceRegisterResponseDTO>>notNull())).thenReturn(Mono.just(post));
+        mockMvc.perform(get(InstanceController.BASE_NAME + InstanceController.INSTANCE_STATUS_ENDPOINT, ENCODED_SOCKET_EXAMPLE)).andExpect(status().isOk());
+    }
+}
