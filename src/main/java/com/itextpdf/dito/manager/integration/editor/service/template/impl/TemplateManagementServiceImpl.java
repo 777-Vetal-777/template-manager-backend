a2642e48aa478b46a43762f06719daadbd2e9a66
Yaroslav_Maievskiy <y.maievskyi@andersenlab.com>
2021-03-23 16:03:07 +0200

    Fix related issue: cannot create new version from editor.
    DTM-2572

--- src/main/java/com/itextpdf/dito/manager/integration/editor/service/template/impl/TemplateManagementServiceImpl.java
+++ src/main/java/com/itextpdf/dito/manager/integration/editor/service/template/impl/TemplateManagementServiceImpl.java
@@ -3,6 +3,7 @@ package com.itextpdf.dito.manager.integration.editor.service.template.impl;
 import com.itextpdf.dito.manager.component.encoder.Encoder;
 import com.itextpdf.dito.manager.dto.resource.ResourceTypeEnum;
 import com.itextpdf.dito.manager.entity.TemplateTypeEnum;
+import com.itextpdf.dito.manager.entity.UserEntity;
 import com.itextpdf.dito.manager.entity.resource.ResourceEntity;
 import com.itextpdf.dito.manager.entity.resource.ResourceFileEntity;
 import com.itextpdf.dito.manager.entity.template.TemplateEntity;
@@ -11,10 +12,12 @@ import com.itextpdf.dito.manager.exception.integration.InconsistencyException;
 import com.itextpdf.dito.manager.integration.editor.dto.ResourceIdDTO;
 import com.itextpdf.dito.manager.integration.editor.mapper.resource.ResourceLeafDescriptorMapper;
 import com.itextpdf.dito.manager.integration.editor.service.template.TemplateManagementService;
+import com.itextpdf.dito.manager.repository.template.TemplateFileRepository;
 import com.itextpdf.dito.manager.repository.template.TemplateRepository;
 import com.itextpdf.dito.manager.service.resource.ResourceService;
 import com.itextpdf.dito.manager.service.template.TemplateLoader;
 import com.itextpdf.dito.manager.service.template.TemplateService;
+import com.itextpdf.dito.manager.service.user.UserService;
 import com.itextpdf.dito.sdk.core.dependency.api.TemplateDependency;
 import com.itextpdf.dito.sdk.core.dependency.retriever.template.DefaultTemplateDependenciesRetriever;
 import com.itextpdf.dito.sdk.core.dependency.retriever.template.TemplateDependenciesRetriever;
@@ -25,6 +28,7 @@ import javax.transaction.Transactional;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
@@ -37,27 +41,33 @@ public class TemplateManagementServiceImpl implements TemplateManagementService 
     private final TemplateAssetRetriever resourceAssetRetriever;
     private final TemplateAssetRetriever templateAssetRetriever;
     private final TemplateRepository templateRepository;
+    private final TemplateFileRepository templateFileRepository;
     private final ResourceLeafDescriptorMapper resourceLeafDescriptorMapper;
     private final ResourceService resourceService;
     private final TemplateLoader templateLoader;
+    private final UserService userService;
     private final Encoder encoder;
 
     public TemplateManagementServiceImpl(final TemplateService templateService,
                                          final TemplateAssetRetriever resourceAssetRetriever,
                                          final TemplateAssetRetriever templateAssetRetriever,
                                          final TemplateRepository templateRepository,
+                                         final TemplateFileRepository templateFileRepository,
                                          final ResourceService resourceService,
                                          final TemplateLoader templateLoader,
                                          final ResourceLeafDescriptorMapper resourceLeafDescriptorMapper,
-                                         final Encoder encoder) {
+                                         final Encoder encoder,
+                                         final UserService userService) {
         this.templateService = templateService;
         this.resourceAssetRetriever = resourceAssetRetriever;
         this.templateAssetRetriever = templateAssetRetriever;
+        this.templateFileRepository = templateFileRepository;
         this.resourceService = resourceService;
         this.templateRepository = templateRepository;
         this.templateLoader = templateLoader;
         this.encoder = encoder;
         this.resourceLeafDescriptorMapper = resourceLeafDescriptorMapper;
+        this.userService = userService;
     }
 
     @Override
@@ -74,9 +84,16 @@ public class TemplateManagementServiceImpl implements TemplateManagementService 
     @Transactional(rollbackOn = InconsistencyException.class)
     public TemplateEntity createNewVersion(final String name, final byte[] data, final String email,
                                            final String newName, final String comment) {
-        final TemplateEntity templateEntity = templateService.createNewVersion(name, data, email, comment, newName, null);
+        final TemplateEntity existingTemplate = templateService.get(name);
+        final TemplateEntity templateEntity;
+        final TemplateFileEntity firstFileOfTemplate = templateFileRepository.findFirstByTemplate_IdOrderByVersionDesc(existingTemplate.getId());
+        if (firstFileOfTemplate.getVersion() == 1 && Arrays.equals(firstFileOfTemplate.getData(), templateLoader.load())){
+            templateEntity = provideFirstVersionTemplateCreation(existingTemplate, data, comment, email);
+        }
+        else {
+            templateEntity = templateService.createNewVersion(name, data, email, comment, newName, null);
+        }
         templateEntity.getLatestFile().setResourceFiles(provideConsistency(templateEntity.getLatestFile().getData()));
-        provideFirstVersionTemplateCreation(templateEntity);
         return templateRepository.save(templateEntity);
     }
 
@@ -97,26 +114,18 @@ public class TemplateManagementServiceImpl implements TemplateManagementService 
         return templateService.delete(templateName);
     }
 
-	private void provideFirstVersionTemplateCreation(final TemplateEntity templateEntity) {
-		// versions sorted DESC in TemplateEntity
-		if (templateEntity.getFiles().size() == 2
-				&& Arrays.equals(templateEntity.getFiles().get(1).getData(), templateLoader.load())) {
-			// updating empty version to editor version
-			final TemplateFileEntity templateFileEditorVersion = templateEntity.getFiles().get(1);
-			final TemplateFileEntity templateFileEmptyVersion = templateEntity.getFiles().get(0);
-			templateFileEditorVersion.setData(templateFileEmptyVersion.getData());
-			templateFileEditorVersion.setAuthor(templateFileEmptyVersion.getAuthor());
-			templateFileEditorVersion.setComment(templateFileEmptyVersion.getComment());
-			templateFileEditorVersion.setCreatedOn(templateFileEmptyVersion.getCreatedOn());
-			templateFileEditorVersion.setDataCollectionFile(templateFileEmptyVersion.getDataCollectionFile());
-			templateFileEditorVersion.setDeployed(templateFileEmptyVersion.getDeployed());
-			templateFileEditorVersion.setModifiedOn(templateFileEmptyVersion.getModifiedOn());
-			templateFileEditorVersion.setResourceFiles(templateFileEmptyVersion.getResourceFiles());
-			//removing editor version
-			templateEntity.getFiles().remove(0);
-		}
-	}
-	
+	private TemplateEntity provideFirstVersionTemplateCreation(final TemplateEntity templateEntity, final byte[] data, final String comment, final String userEmail) {
+        final UserEntity creatorEntity = userService.findActiveUserByEmail(userEmail);
+        final TemplateFileEntity templateFileEntity = templateFileRepository.findFirstByTemplate_IdOrderByVersionDesc(templateEntity.getId());
+            templateFileEntity.setData(data);
+            templateFileEntity.setComment(comment);
+            templateFileEntity.setCreatedOn(new Date());
+            templateFileEntity.setModifiedOn(new Date());
+            templateFileEntity.setAuthor(creatorEntity);
+            templateEntity.setLatestFile(templateFileEntity);
+            return templateEntity;
+    }
+
     private Set<ResourceFileEntity> provideConsistency(final byte[] data) {
         final TemplateDependenciesRetriever retriever = new DefaultTemplateDependenciesRetriever(templateAssetRetriever,
                 resourceAssetRetriever);
